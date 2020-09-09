73605499fb102614b530d5df601ba30c5f72bca8
Hasan <systemry420@gmail.com>
2020-09-09 11:37:12 +0300

    drop data dynamically into page

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const APIKEY = '04c35731a5ee918f014970082a0088b1';
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
 const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const main = document.querySelector('main');
 getMovies();
- 
+
 async function getMovies() {
     const response = await fetch(APIURL);
     const respData = await response.json();
@@ -10,10 +11,18 @@ async function getMovies() {
     console.log(respData);
 
     respData.results.forEach(movie => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add('movie');
 
-        document.body.appendChild(img);
+        movieEl.innerHTML = `
+            <img src="${IMGPATH + movie.poster_path}" alt="${movie.original_title}">
+            <div class="movie-info">
+                <h3>${movie.original_title}</h3>
+                <span>${movie.vote_average}</span>
+            </div>
+        `;
+
+        main.appendChild(movieEl)
     });
     return respData;
 }
